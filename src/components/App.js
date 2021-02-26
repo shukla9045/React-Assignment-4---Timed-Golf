5294ea134de5f0c602f8eca82e5095f86b027f04
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-26 13:17:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,96 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = {
+      renderBall: false,
+      time: 0,
+      x: 0,
+      y: 0,
+      ballPosition: {
+        left: "0px",
+        top: "0px"
+      }
+    };
+    this.renderChoice = this.renderChoice.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKey = this.handleKey.bind(this);
   }
-  componentDidMount() {
-    
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+    document.addEventListener("keydown", this.handleKey);
+    this.interval = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
   }
 
-  componentWillUnmount() {
-    
+  renderChoice() {
+    return (
+      <div>
+        <div className="ball" style={this.state.ballPosition}></div>
+        <div className="hole"></div>
+        <div className="heading-timer">{this.state.time}</div>
+
+        <button
+          className="start ballProvider"
+          onClick={this.buttonClickHandler}
+        >
+          Click For Start
+        </button>
+      </div>
+    );
   }
 
+  componentDidMount() {}
 
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.interval);
+      document.removeEventListener("keydown", this.handleKey);
+    }
+  }
+
+  handleKey(event) {
+    if (event.keyCode === 39) {
+      this.setState({ x: this.state.x + 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px"
+          }
+        })
+      );
+    } else if (event.keyCode === 40) {
+      this.setState({ y: this.state.y + 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px"
+          }
+        })
+      );
+    } else if (event.keyCode === 38) {
+      this.setState({ y: this.state.y - 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px"
+          }
+        })
+      );
+    } else if (event.keyCode === 37) {
+      this.setState({ x: this.state.x - 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px"
+          }
+        })
+      );
+    }
+  }
 
   render() {
-    return (
- <>
-</>
-    );
+    return <div className="playground">{this.renderChoice()}</div>;
   }
 }
 
