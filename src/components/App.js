b67fa00adf4619f63149adb4a84045168311fc7a
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-03-30 13:25:56 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,100 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-var clearSetInterval = 0;
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-    this.moveBall = this.moveBall.bind(this);
-    this.setup = this.setup.bind(this);
+    this.state = { 
+      time: 0, 
+      x: 0, 
+      y: 0,
+      renderBall: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.timmer = this.timmer.bind(this);
+
   }
-  setup(event) {
-    switch (event.keyCode) {
-      case 37:
-        this.setState({ x: this.state.x - 5 });
-        break;
-      case 38:
-        this.setState({ y: this.state.y - 5 });
-        break;
-      case 39:
-        this.setState({ x: this.state.x + 5 });
-        break;
-      case 40:
-        this.setState({ y: this.state.y + 5 });
-        break;
-      default:
-        break;
+
+
+  componentDidMount() {
+        window.addEventListener('keydown', (e) => {
+          switch (e.key) {
+              case 'ArrowRight':
+                this.setState({
+                    x: this.state.x + 5,
+                    y: this.state.y + 0,
+                })
+                break;
+
+              case 'ArrowDown':
+                this.setState({
+                  x: this.state.x + 0,
+                  y: this.state.y + 5,
+                })
+                
+                break;
+
+              case 'ArrowLeft':
+                this.setState({
+                  x: this.state.x - 5,
+                  y: this.state.y + 0,
+                })
+                break;
+
+              case 'ArrowUp':
+                this.setState({
+                  x: this.state.x + 0,
+                  y: this.state.y - 5,
+                })
+                break;
+          }
+      });
+
+      
+      if(this.state.x == 250 && this.state.y == 250){
+        console.log("I AM FROM IF");
+        window.clearInterval(interval);
+      }
+
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      time : 0,
+      // renderBall: false,
+    })
+  }
+
+  timmer(){
+
+    if(this.state.x!=250 && this.state.y!=250){
+      this.setState({
+        time: this.state.time + 1,
+      })
     }
-    if (this.state.x === 250 && this.state.y === 250) {
-      document.removeEventListener("keydown", this.setup);
-      this.interval();
+    else{
+      
     }
   }
 
-  interval() {
-    if (this.state.time === 0)
-      clearSetInterval = setInterval(() => {
-        this.setState({ time: this.state.time + 1 });
-      }, 1000);
-    if (this.state.x === 250 && this.state.y === 250)
-      clearInterval(clearSetInterval);
+
+  handleClick(){
+    // console.log("Start Button Clicked");
+    var interval = window.setInterval(this.timmer,1000);
+
+    this.setState({
+      renderBall : true,
+    })
   }
 
-  moveBall() {
-    if (this.state.time === 0) {
-      this.interval();
-      document.addEventListener("keydown", this.setup);
-    }
-  }
-  // componentDidMount() {
-
-  // }
-
-  // componentWillUnmount() {
-
-  // }
 
   render() {
     return (
       <>
-        <div
-          className="ball"
-          style={{ left: this.state.x, top: this.state.y }}
-        ></div>
-        <div className="heading-timer">{this.state.time}</div>
-        <button className="start" onClick={this.moveBall}>
-          start
-        </button>
+        <div className="ball" style={{left : this.state.x + "px" , top : this.state.y + "px"}}></div>
         <div className="hole"></div>
+        <h1 className="heading-timer">{this.state.time}</h1>
+        <button className="start" onClick={this.handleClick}>Start</button>
+
       </>
     );
   }
